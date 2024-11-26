9930c407023f920c49ef7663c538e042fde579c1
ABUBAKAR139-hue <abubakarummaabba2021@gmail.com>
2024-11-26 07:06:45 +0100

    MOD App structure, adding containers to the individual Views rather than general app

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
 
     <!-- Main Content Section -->
     <main class="py-12">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="">
         <router-view></router-view>
         <!-- Dynamic component loading -->
       </div>
--- src/views/LessonsPage.vue
+++ src/views/LessonsPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <!-- Passing props and handle updates via events -->
     <SortOptions
       :sortAttribute="sortAttribute"
